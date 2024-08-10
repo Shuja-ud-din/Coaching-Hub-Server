b2c9315179d1d9d88ddb01281328608429aef3ef
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-08-10 09:39:11 -0400

    userModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
-
+import Joi from 'joi';
+import joi from 'joi';
 import mongoose from 'mongoose';
 
 const schema = mongoose.Scheme({
@@ -19,6 +20,65 @@ const schema = mongoose.Scheme({
     password: {
         type: String,
         required: true,
+    },
+    role: {
+        type: String,
+        default: 'Customer',
+    },
+    otp: {
+        type: String,
+    },
+    isValid: {
+        type: Boolean,
+        default: false,
+    },
+    profilePicture: {
+        type: String,
+        default: null,
+    },
+    notifications: {
+        type: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Notification',
+            },
+        ],
+        default: [],
     }
+})
 
+const User = mongoose.model("User", schema);
+
+const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required(),
+    phoneNumber: joi.number().required(),
+    password: joi.string().required(),
+    profilePicture: joi.string().optional(),
+})
+
+const loginSchema = joi.object({
+    phoneNumber: joi.number().required(),
+    password: joi.string().required(),
+})
+
+const updateSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required(),
+    phoneNumber: joi.number().required(),
+    password: joi.string().required(),
+    profilePicture: joi.string().optional(),
+})
+
+const toggleValidityBody = Joi.object({
+    isValid: Joi.boolean().required(),
+})
+
+const verifyUserBody = Joi.object({
+    otp: Joi.string().required(),
+    phoneNumber: Joi.string().required(),
+})
+
+const generateOTPBody = Joi.object({
+    phoneNumber: Joi.string().required(),
 })
