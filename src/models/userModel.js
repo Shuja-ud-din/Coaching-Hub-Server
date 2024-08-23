9a3d4bf4d4c74c7284e540c44927eae80f5c9231
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-08-23 13:03:28 -0600

    update

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,93 +1,95 @@
-import Joi from 'joi';
-import joi from 'joi';
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import joi from "joi";
+import Joi from "joi";
 
-const schema = mongoose.Scheme({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: 'Customer',
-    },
-    otp: {
-        type: String,
-    },
-    isValid: {
-        type: Boolean,
-        default: false,
-    },
-    profilePicture: {
-        type: String,
-        default: null,
-    },
-    notifications: {
-        type: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Notification',
-            },
-        ],
-        default: [],
-    }
-})
+const schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "Customer",
+  },
+  otp: {
+    type: String,
+  },
+  isValid: {
+    type: Boolean,
+    default: false,
+  },
+  profilePicture: {
+    type: String,
+    default: null,
+  },
+  notifications: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Notification",
+      },
+    ],
+    default: [],
+  },
+});
 
 const User = mongoose.model("User", schema);
 
+// schema
 const userSchema = joi.object({
-    name: joi.string().required(),
-    email: joi.string().required(),
-    phoneNumber: joi.number().required(),
-    password: joi.string().required(),
-    profilePicture: joi.string().optional(),
-})
+  name: joi.string().required(),
+  email: joi.string().required(),
+  phoneNumber: joi.number().required(),
+  password: joi.string().required(),
+  profilePicture: joi.string().optional(),
+});
 
 const loginSchema = joi.object({
-    phoneNumber: joi.number().required(),
-    password: joi.string().required(),
-})
+  phoneNumber: joi.number().required(),
+  password: joi.string().required(),
+});
 
 const updateSchema = joi.object({
-    name: joi.string().required(),
-    email: joi.string().required(),
-    phoneNumber: joi.number().required(),
-    password: joi.string().required(),
-    profilePicture: joi.string().optional(),
-})
+  name: joi.string().required(),
+  email: joi.string().required(),
+  phoneNumber: joi.number().required(),
+  password: joi.string().required(),
+  profilePicture: joi.string().optional(),
+});
 
 const toggleValidityBody = Joi.object({
-    isValid: Joi.boolean().required(),
-})
+  isValid: Joi.boolean().required(),
+});
 
 const verifyUserBody = Joi.object({
-    otp: Joi.string().required(),
-    phoneNumber: Joi.string().required(),
-})
+  otp: Joi.string().required(),
+  phoneNumber: Joi.string().required(),
+});
 
 const generateOTPBody = Joi.object({
-    phoneNumber: Joi.string().required(),
-})
+  phoneNumber: Joi.string().required(),
+});
+
 export default User;
 export {
-    userSchema,
-    loginSchema,
-    updateSchema,
-    toggleValidityBody,
-    verifyUserBody,
-    generateOTPBody,
-}
+  userSchema,
+  loginSchema,
+  updateSchema,
+  toggleValidityBody,
+  verifyUserBody,
+  generateOTPBody,
+};
