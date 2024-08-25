90c7e4254a5753cfc5758eaa0aa94e5aa985db03
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-08-25 12:43:57 -0600

    update

---
+++ src/utils/sendNotification.js
@@ -0,0 +1,36 @@
+import Notification from "../models/notificationModel.js";
+
+export const sendNotificationToAdmin = async (title, message) => {
+  const superAdmin = await User.findOne({ role: "Super Admin" });
+
+  const notification = await Notification.create({
+    title,
+    message,
+    user: superAdmin._id,
+  });
+
+  const unreadNotifications = await Notification.countDocuments({
+    user: superAdmin._id,
+    isRead: false,
+  });
+
+  const receivers = connectedUsers.filter(
+    (user) => user.role === "Admin" || user.role === "Super Admin"
+  );
+
+  if (receivers.length > 0) {
+    for (let receiver of receivers) {
+      io.to(receiver.socketId).emit("notification", {
+        unreadNotifications,
+
+        notification: {
+          id: notification._id,
+          title: notification.title,
+          message: notification.message,
+          isRead: notification.isRead,
+          createdAt: notification.createdAt,
+        },
+      });
+    }
+  }
+};
