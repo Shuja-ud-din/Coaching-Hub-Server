a89a87b1d8949596d37d83f6d88e498d46036ed4
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-09-12 15:48:24 -0500

    todos controller and services and routes in appointment routes

---
+++ src/controller/todos.controller.js
@@ -0,0 +1,68 @@
+import httpStatus from "http-status";
+import { ApiError } from "../errors/ApiError.js";
+import {
+  addTodos,
+  deleteTodo,
+  getAllTodos,
+  updateTodo,
+} from "../services/todos.service.js";
+
+const addTodosHandler = async (req, res) => {
+  try {
+    const { name, description, status } = req.body;
+    const { id: appointmentId } = req.params;
+    const todo = await addTodos(appointmentId, name, description, status);
+    res.status(201).json({ success: true, data: todo });
+  } catch (error) {
+    throw new ApiError(
+      error.statusCode || httpStatus.BAD_REQUEST,
+      error.message || "Internal server error"
+    );
+  }
+};
+
+const getAllTodosHandler = async (req, res) => {
+  try {
+    const { id: appointmentId } = req.params;
+    const todos = await getAllTodos(appointmentId);
+    res.status(200).json({ success: true, data: todos });
+  } catch (error) {
+    throw new ApiError(
+      error.statusCode || httpStatus.BAD_REQUEST,
+      error.message || "Internal server error"
+    );
+  }
+};
+
+const updateTodosHandler = async (req, res) => {
+  try {
+    const { id: todoId } = req.params;
+    const updatedTodo = await updateTodo(todoId, req.body);
+    res.status(200).json({ success: true, data: updatedTodo });
+  } catch (error) {
+    throw new ApiError(
+      error.statusCode || httpStatus.BAD_REQUEST,
+      error.message || "Internal server error"
+    );
+  }
+};
+
+const deleteTodosHandler = async (req, res) => {
+  try {
+    const { id: todoId } = req.params;
+    const deletedTodo = await deleteTodo(todoId);
+    res.status(200).json({ success: true, data: deletedTodo });
+  } catch (error) {
+    throw new ApiError(
+      error.statusCode || httpStatus.BAD_REQUEST,
+      error.message || "Internal server error"
+    );
+  }
+};
+
+export {
+  addTodosHandler,
+  getAllTodosHandler,
+  updateTodosHandler,
+  deleteTodosHandler,
+};
